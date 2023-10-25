36fa179bf46c627f699b54345a51b507b1693b6b
Babatunde Peter <babatundepeter9@gmail.com>
2023-10-25 07:46:06 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let output = document.querySelector(".output");
+let result = document.querySelector(".result");
+let input = document.querySelector(".input");
+
+input.addEventListener("click", () => {
+    result.innerText = input.value + "0%";
+    output.getElementsByClassName.borderRadius = input.value + "%";
+
+})
